39420566ea0575bcccc0e6e53879617a7cb09a59
Mark Lhester Pile <pilemarklhester.work@gmail.com>
2026-06-25 09:53:13 +0800

    page navigation

---
+++ components/headerNav.js
@@ -0,0 +1,31 @@
+const nav = document.querySelector("ul");
+const pill = document.querySelector(".pill");
+const links = document.querySelectorAll("ul li a");
+
+function movePill(link) {
+  pill.style.left = link.offsetLeft + "px";
+  pill.style.width = link.offsetWidth + "px";
+}
+
+function setActive(el) {
+  links.forEach((link) => link.classList.remove("active"));
+  el.classList.add("active");
+  movePill(el);
+}
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    setActive(link);
+  });
+});
+
+window.addEventListener("load", () => {
+  const active = document.querySelector("ul li a.active") || links[0];
+  movePill(active);
+});
+
+window.addEventListener("resize", () => {
+  const active = document.querySelector("ul li a.active");
+  if (active) movePill(active);
+});
